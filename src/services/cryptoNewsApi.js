75f9e0dec1aca6afc00d1d582efeb4dafdb8602c
mayanksomani07 <83702471+mayanksomani7@users.noreply.github.com>
2023-05-10 22:26:43 +0530

    initial commit

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+  'x-bingapis-sdk': 'true',
+  'x-rapidapi-key': '98829bbadfmsh72259088416f3acp1e2464jsn01accbd61061',
+  'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+};
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com'
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
